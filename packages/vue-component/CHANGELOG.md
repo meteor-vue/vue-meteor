ff6ff738fca6c383350b3e1ac375ebbb3f9d92d2
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-05-30 15:14:17 +0200

    Bump

---
+++ packages/vue-component/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 0.0.2 - 2016/05/30
+
+ - Removed autoprefixer postcss plugin due to a huge hit on the plugin loading time.
--- packages/vue-component/package.js
+++ packages/vue-component/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-component',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'VueJS single-file components',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md'
