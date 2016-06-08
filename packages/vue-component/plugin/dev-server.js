9c7ac4ef5a54e12896cb20685c383acdb35ba310
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-06-08 13:31:46 +0200

    Dev checks

--- packages/vue-component/plugin/dev-server.js
+++ packages/vue-component/plugin/dev-server.js
@@ -1,20 +1,24 @@
 import http from 'http';
 import socket_io from 'socket.io';
+import {Meteor} from 'meteor/meteor';
 
 if(global._dev_server_http) {
   global._dev_server_http.close();
+  delete global._dev_server_http;
 }
 
-var server = http.createServer();
-var io = socket_io(server);
+if(Meteor.isDevelopment) {
+  var server = http.createServer();
+  var io = socket_io(server);
 
-PORT = 4242;
+  PORT = 4242;
 
-try {
-  server.listen(PORT);
-  console.log(`\nDev server listening on port ${PORT}`);
-  global._dev_server = io;
-  global._dev_server_http = server;
-} catch(e) {
-  console.log(e);
+  try {
+    server.listen(PORT);
+    console.log(`\nDev server (vue-components) listening on port ${PORT}`);
+    global._dev_server = io;
+    global._dev_server_http = server;
+  } catch(e) {
+    console.log(e);
+  }
 }
