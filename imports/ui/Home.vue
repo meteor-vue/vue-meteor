12e3b8d1fb47dd5d228a21dc54059a9f4a49efbb
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-06-16 21:23:38 +0200

    Home misc

--- imports/ui/Home.vue
+++ imports/ui/Home.vue
@@ -1,19 +1,19 @@
 <template lang="jade">
 .home
-  h1 Welcome Home!
-  p Let's have some fun with vuejs &amp; meteor
+  h1 Welcome Home
+  p Create awesome apps with vuejs &amp; meteor! 
+    a(href="https://github.com/Akryum/meteor-vue-component") More info
   img.logo(src="/img/vue.png")
   img.logo(src="/img/meteor.png")
 </template>
 
-<style scoped>
-p {
+<style scoped lang="less">
+.home {
   text-align: center;
 }
 
 img.logo {
   max-width: 101px;
-  display: block;
-  margin: 12px auto;
+  margin: 12px;
 }
 </style>
