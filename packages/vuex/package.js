415e6e3f8104e288ed54f6e9266a03096e0b96ea
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-07-01 11:03:02 +0200

    vuex hotfixw

--- packages/vuex/package.js
+++ packages/vuex/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vuex',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'State management with vuex for Meteor',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md'
@@ -8,12 +8,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.3');
-  api.use('isobuild:compiler-plugin@1.0.0');
   api.use('ecmascript');
   api.use('akryum:npm-check@0.0.2');
   api.use('akryum:vue@1.0.3');
   api.mainModule('client/client.js', 'client');
-  api.export('StoreModule', 'client');
+  api.export(['StoreModule', 'StoreSubModule'], 'client');
 });
 
 Npm.depends({
