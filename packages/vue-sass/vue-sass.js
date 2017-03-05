831b2d75569e565f5172e7c3938b8acb916fc279
Nathan Reid <nathan-code@nathantreid.com>
2017-03-05 13:49:00 -0600

    automatically discover import paths without extensions. Check for underscore-prefixed variants as well.

--- packages/vue-sass/vue-sass.js
+++ packages/vue-sass/vue-sass.js
@@ -21,7 +21,8 @@ function resolveImport(dependencyManager) {
       resolvedFilename = path.resolve(currentDirectory, url);
     }
 
-    if (!fs.existsSync(resolvedFilename)) {
+    resolvedFilename = discoverImportPath(resolvedFilename);
+    if (resolvedFilename === null) {
       done(new Error('Unknown import (file not found): ' + url));
     } else {
       dependencyManager.addDependency(resolvedFilename);
@@ -31,6 +32,26 @@ function resolveImport(dependencyManager) {
       });
     }
   };
+}
+
+function discoverImportPath(importPath) {
+  const potentialPaths = [importPath];
+  const potentialFileExtensions = ["scss", "sass"];
+
+  if (!path.extname(importPath)) {
+    potentialFileExtensions.forEach(extension => potentialPaths.push(`${importPath}.${extension}`));
+  }
+  if (path.basename(importPath)[0] !== '_') {
+    [].concat(potentialPaths).forEach(potentialPath => potentialPaths.push(`${path.dirname(potentialPath)}/_${path.basename(potentialPath)}`));
+  }
+
+  for (let i = 0, potentialPath = potentialPaths[i]; i < potentialPaths.length; i++, potentialPath = potentialPaths[i]) {
+    if (fs.existsSync(potentialPaths[i]) && fs.lstatSync(potentialPaths[i]).isFile()) {
+      return potentialPath;
+    }
+  }
+
+  return null;
 }
 
 function decodeFilePath(filePath) {
