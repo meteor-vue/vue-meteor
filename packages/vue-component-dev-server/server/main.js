6c5e5ec20798f2d8d880ea5d6d6632d978426db6
Guillaume Chau <alphadelta.fg@gmail.com>
2017-04-27 17:20:49 +0200

    Fix automatic ip detection

--- packages/vue-component-dev-server/server/main.js
+++ packages/vue-component-dev-server/server/main.js
@@ -1,11 +1,40 @@
-import localIp from 'local-ipv4-address'
+import os from 'os'
 
 function getMeteorPort() {
-  const reg = /:\/\/.+:(\d+)/gi;
-  const result = reg.exec(Meteor.absoluteUrl());
+  const reg = /:\/\/.+:(\d+)/gi
+  const result = reg.exec(Meteor.absoluteUrl())
   if(result && result.length >= 2) {
-    return parseInt(result[1]) + 3;
+    return parseInt(result[1]) + 3
   }
+}
+
+function getLocalIp () {
+  const ifaces = os.networkInterfaces()
+
+  let ip
+  for (const key of Object.keys(ifaces)) {
+    const interfaces = ifaces[key]
+    for (const iface of interfaces) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      } else {
+        ip = iface.address
+        break
+      }
+    }
+    if (ip) {
+      break
+    }
+  }
+
+  if (!ip) {
+    console.warn(`[HMR] No local IP detected. If you want to connect from a remote device, set the local IP with the 'HMR_URL' env. variable.`)
+    ip = '127.0.0.1'
+  } else {
+    // console.warn(`[HMR] Local IP detected: '${ip}'. If you have issues connecting from a remote device, set the local IP with the 'HMR_URL' env. variable.`)
+  }
+
+  return ip
 }
 
 const localIpSync = Meteor.wrapAsync(cb => {
@@ -17,11 +46,11 @@ const localIpSync = Meteor.wrapAsync(cb => {
 })
 
 // to define only dev port with same url
-const PORT = parseInt(process.env.HMR_PORT) || parseInt(process.env.VUE_DEV_SERVER_PORT) || getMeteorPort() || 3003;
+const PORT = parseInt(process.env.HMR_PORT) || parseInt(process.env.VUE_DEV_SERVER_PORT) || getMeteorPort() || 3003
 
 // to define full url with port (example: https://dev.example.com:8443) or only domain
-const DEVURL = process.env.HMR_URL || process.env.VUE_DEV_SERVER_URL || localIpSync();
+const DEVURL = process.env.HMR_URL || process.env.VUE_DEV_SERVER_URL || getLocalIp()
 
 
 // Client-side config
-__meteor_runtime_config__.VUE_DEV_SERVER_URL = DEVURL.indexOf(':') === -1 ? `${DEVURL}:${PORT}` : DEVURL;
+__meteor_runtime_config__.VUE_DEV_SERVER_URL = `${DEVURL}:${PORT}`
