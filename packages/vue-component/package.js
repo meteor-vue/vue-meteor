a81228cdda9b34a1a834b413301c9e722da408a5
Guillaume Chau <alphadelta.fg@gmail.com>
2017-10-27 17:56:25 +0200

    Version bump

--- packages/vue-component/package.js
+++ packages/vue-component/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-component',
-  version: '0.10.8',
+  version: '0.11.0',
   summary: 'VueJS single-file components that hot-reloads',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md'
@@ -48,5 +48,5 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4.2');
   api.use('isobuild:compiler-plugin@1.0.0');
   api.use('akryum:vue-component-dev-server@0.0.11');
-  api.use('akryum:vue-component-dev-client@0.2.12');
+  api.use('akryum:vue-component-dev-client@0.3.0');
 });
