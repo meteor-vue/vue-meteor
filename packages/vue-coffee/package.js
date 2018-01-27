50e03005aa133b3f44b831f14d444a5e96eb54aa
Geoffrey Booth <GeoffreyBooth@users.noreply.github.com>
2018-01-27 08:15:44 -0800

    Bump CoffeeScript version to 2.0.3 (#271)

--- packages/vue-coffee/package.js
+++ packages/vue-coffee/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-coffee',
-  version: '0.2.0',
+  version: '0.2.1',
   summary: 'Add coffee support for vue components',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: 'vue-component-coffee',
-  use: ['ecmascript@0.8.3', 'coffeescript-compiler@2.0.2_1'],
+  use: ['ecmascript@0.8.3', 'coffeescript-compiler@2.0.3_1'],
   sources: ['vue-coffee.js']
 });
 
