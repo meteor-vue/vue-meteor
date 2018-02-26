cf53b0a2b03e8463734266ce3d384b8d4c7f5792
Guillaume Chau <alphadelta.fg@gmail.com>
2018-02-26 23:43:08 +0100

    Versions bump

--- packages/vue-component/package.js
+++ packages/vue-component/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-component',
-  version: '0.13.0',
+  version: '0.13.1',
   summary: 'VueJS single-file components that hot-reloads',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
@@ -45,5 +45,5 @@ Package.registerBuildPlugin({
 Package.onUse(function (api) {
   api.use('isobuild:compiler-plugin@1.0.0')
   api.use('akryum:vue-component-dev-server@0.1.0')
-  api.use('akryum:vue-component-dev-client@0.4.0')
+  api.use('akryum:vue-component-dev-client@0.4.1')
 })
--- packages/vue-ssr/package.js
+++ packages/vue-ssr/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-ssr',
-  version: '0.3.0',
+  version: '0.3.1',
   summary: 'Render Vue server-side',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
