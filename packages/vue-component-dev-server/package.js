0c9a2211d6f424435ae29e22456b349dac0a0cbe
Guillaume Chau <alphadelta.fg@gmail.com>
2018-12-02 17:18:28 +0100

    chore: bump versions

--- packages/vue-component-dev-server/package.js
+++ packages/vue-component-dev-server/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-component-dev-server',
-  version: '0.1.2',
+  version: '0.1.3',
   summary: 'Dev server for vue hot-reloading',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
--- packages/vue-component/package.js
+++ packages/vue-component/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-component',
-  version: '0.14.1',
+  version: '0.14.2',
   summary: 'VueJS single-file components that hot-reloads',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
@@ -45,6 +45,6 @@ Package.registerBuildPlugin({
 
 Package.onUse(function (api) {
   api.use('isobuild:compiler-plugin@1.0.0')
-  api.use('akryum:vue-component-dev-server@0.1.2')
-  api.use('akryum:vue-component-dev-client@0.4.3')
+  api.use('akryum:vue-component-dev-server@0.1.3')
+  api.use('akryum:vue-component-dev-client@0.4.4')
 })
