ddb9efd13f6ae39fb1dc6cecb2f67972e8a9c886
Guillaume Chau <alphadelta.fg@gmail.com>
2019-05-11 01:18:45 +0200

    chore: bump version

--- packages/vue-sass/package.js
+++ packages/vue-sass/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-sass',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Add sass and scss support for vue components',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
