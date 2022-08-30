4cc6008a637631af5719fefcffa1a9a223761598
Iván Cabrera <39581375+diavrank@users.noreply.github.com>
2022-08-30 05:12:00 -0400

    Fix: Update node-sass to make it compatible with Node 14 (#433)

--- packages/vue-sass/package.js
+++ packages/vue-sass/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-sass',
-  version: '0.1.2',
+  version: '0.1.3',
   summary: 'Add sass and scss support for vue components',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
@@ -15,7 +15,7 @@ Package.registerBuildPlugin({
     'vue-sass.js',
   ],
   npmDependencies: {
-    'node-sass': '4.12.0',
+    'node-sass': '4.14.0',
     'meteor-project-path': '0.0.3',
   },
 })
