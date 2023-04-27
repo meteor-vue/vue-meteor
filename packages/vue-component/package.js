689e512541dd1121bebf15af1b6cc41064398b51
Guillaume Chau <guillaume.b.chau@gmail.com>
2023-04-27 21:36:23 +0200

    chore: bump version

--- packages/vue-component/package.js
+++ packages/vue-component/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-component',
-  version: '0.15.2',
+  version: '0.15.3',
   summary: 'VueJS single-file components that hot-reloads',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
