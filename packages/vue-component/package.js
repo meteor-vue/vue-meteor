c462d59102b0db3f10ae5a1102c0f7aa793cf597
Guillaume Chau <guillaume.b.chau@gmail.com>
2023-04-27 21:26:11 +0200

    revert: package version

--- packages/vue-component/package.js
+++ packages/vue-component/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'akryum:vue-component',
-  version: '1.0.0',
+  version: '0.15.2',
   summary: 'VueJS single-file components that hot-reloads',
   git: 'https://github.com/Akryum/meteor-vue-component',
   documentation: 'README.md',
